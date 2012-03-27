ec1f5f5da0a3e2fa43160030e5dab06882588437
Robert Wertman <robert.wertman@gmail.com>
2012-03-27 17:44:24 -0400

    Added skeleton image processing.

--- include/image.h
+++ include/image.h
@@ -3,6 +3,13 @@
 
 #include "model.h"
 
+typedef struct {
+	unsigned char r;
+	unsigned char g;
+	unsigned char b;
+} pixel_t;
+
 void make_image(model_t *);
+static pixel_t* pos_to_pixel(int, int, int, pixel_t *);
 
 #endif
--- src/image.c
+++ src/image.c
@@ -1,10 +1,25 @@
 #include <stdlib.h>
 
+#include "Alloc.h"
+#include "log.h"
+
 #include "image.h"
 #include "model.h"
+#include "projection.h"
 
 void make_image(model_t *model) {
-	if (model == NULL) {
-		exit(7);
+	proj_t  *proj  = model->proj;
+	int     width  = proj->win_size_pixel[0];
+	int     height = proj->win_size_pixel[1];
+	pixel_t *image = Malloc((size_t) (width * height) * sizeof(pixel_t));
+
+	for (int row = 0; row < width; ++row) {
+		for (int col = 0; col < height; ++col) {
+			*(pos_to_pixel(row, col, height, image)) = (pixel_t) {255, 0, 0};
+		}
 	}
+}
+
+static inline pixel_t* pos_to_pixel(int row, int col, int height, pixel_t *base) {
+	return (row * height + col) + base;
 }
