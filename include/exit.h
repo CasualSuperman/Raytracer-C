291930eb15136010e660037a9d213a056ecc9e35
Robert Wertman <robert.wertman@gmail.com>
2012-03-30 13:59:54 -0400

    Commented exit.h

--- include/exit.h
+++ include/exit.h
@@ -1,9 +1,13 @@
 #ifndef EXIT_H
 #define EXIT_H
 
+/*
+ * These are the various exit codes used by the program.
+ * EXIT_SUCCESS is already defined, it is simply included in a comment here for
+ * completeness.
+ */
 enum {
-    // This is already defined in a macro; it is included here for completeness.
- /* EXIT_SUCCESS = 0 */
+//  EXIT_SUCCESS = 0,
     EXIT_FAILED_ALLOC = 1,
 	EXIT_BAD_PARAMS   = 2,
 	EXIT_BAD_SCENE    = 3
