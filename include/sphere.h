1bc0a859fd9dc0f0d6769edc928bdef4c88e0c27
Robert Wertman <robert.wertman@gmail.com>
2012-03-30 19:43:44 -0400

    Documented sphere.

--- include/sphere.h
+++ include/sphere.h
@@ -8,7 +8,24 @@ typedef struct {
 	double radius;
 } sphere_t;
 
+/**
+ * init_sphere - Read information from the given file descriptor about an object
+ * with the provided type id.
+ *
+ * @param The file descriptor to read from.
+ * @param The object type id.
+ *
+ * @return An initialized object.
+ */
 obj_t* init_sphere(FILE *, object_id);
+
+/**
+ * dump_sphere - Print information about the provided sphere to the given file
+ * descriptor.
+ *
+ * @param The file descriptor to print to.
+ * @param The object to print information about.
+ */
 void dump_sphere(FILE *, obj_t *);
 
 #endif
