25387a52f392c4c3e55ddb0d0c04fe475c8d4c63
Robert Wertman <robert.wertman@gmail.com>
2012-03-30 18:55:36 -0400

    Planes documented.

--- include/plane.h
+++ include/plane.h
@@ -8,7 +8,24 @@ typedef struct {
 	double normal[3];
 } plane_t;
 
+/**
+ * init_plane - Read information from the given file descriptor about an object
+ * with the provided type id.
+ *
+ * @param The file descriptor to read from.
+ * @param The object type id.
+ *
+ * @return An initialized object.
+ */
 obj_t* init_plane(FILE *, object_id);
+
+/**
+ * dump_plane - Print information about the provided plane to the given file
+ * descriptor.
+ *
+ * @param The file descriptor to print to.
+ * @param The object to print information about.
+ */
 void dump_plane(FILE *, obj_t *);
 
 #endif
