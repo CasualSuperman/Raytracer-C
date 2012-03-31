97a316c4b0f4fcde5944ccdb0ba7f346c621922c
Robert Wertman <robert.wertman@gmail.com>
2012-03-30 20:52:47 -0400

    The projection.

--- include/projection.h
+++ include/projection.h
@@ -9,8 +9,25 @@ typedef struct {
 	double view_point[3];
 } proj_t;
 
+/**
+ * projection_init - Allocates a new proj_t, fills it in using the provided
+ * input stream, and returns the result. Exits on failure.
+ *
+ * @param The number of arguments passed to main.
+ * @param The arguments passed to main.
+ * @param The input stream with information about the proj_t.
+ *
+ * @return The filled in proj_t.
+ */
 proj_t* projection_init(int, char **, FILE *);
 
+/**
+ * projection_dump - Print information about the projection to the given file
+ * descriptor.
+ *
+ * @param The output stream.
+ * @param The proj_t to dump.
+ */
 void projection_dump(FILE *, proj_t*);
 
 #endif
